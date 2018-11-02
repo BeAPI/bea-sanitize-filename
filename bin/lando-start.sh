db12400bf4e41f8aff1a8244d503795376919df6
Nicolas Juen <njuen@beapi.fr>
2018-11-02 14:45:45 +0100

    Finalize implemting tests

--- bin/lando-start.sh
+++ bin/lando-start.sh
@@ -1,11 +1,74 @@
 #!/usr/bin/env bash
 
-composer install --prefer-dist
-mkdir wordpress
-cd wordpress
-wp core download
-wp config create --dbname="wordpress" --dbuser="wordpress" --dbpass="wordpress" --dbhost="database:3306" --dbprefix="wp_"
-wp core install --url="wp.localhost" --title="Test" --admin_user="beapi" --admin_password="beapi" --admin_email="admin@beapi.fr" --skip-email
-wp rewrite structure '/%postname%/' --hard
-mkdir -p wordpress/wp-content/plugins/bea-sanitize-filename
-ln -s /app/bea-sanitize-filename.php /app/wordpress/wp-content/plugins/bea-sanitize-filename/bea-sanitize-filename.php
+# exit on error
+set -e
+
+WP_VERSION="${WP_VERSION:-latest}"
+WP_LOCALE="${WP_LOCALE:-fr_FR}"
+PHP_VERSION="${PHP_VERSION:-7.0}"
+
+cd "$(dirname "$0")"
+cd ..
+
+# Hack - awaiting https://github.com/lando/lando/pull/750
+perl -pi -we "s/^  php: .*/  php: '$PHP_VERSION'/" .lando.yml
+
+
+# BrowsterStack Local installation
+wget https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
+unzip BrowserStackLocal-linux-x64.zip
+rm BrowserStackLocal-linux-x64.zip
+
+lando start -v
+lando wp --version || lando bash test/install-wp-cli.sh
+rm -rf wordpress/[a-z]*
+
+lando wp core download \
+    --path=/app/wordpress/ \
+    --version=$WP_VERSION \
+    --locale=$WP_LOCALE
+
+lando wp config create \
+    --path=/app/wordpress/ \
+    --dbname=wordpress \
+    --dbuser=wordpress \
+    --dbpass=wordpress \
+    --dbhost=database
+
+lando wp config set \
+    --path=/app/wordpress/ \
+    --type=constant \
+    --raw \
+    WP_AUTO_UPDATE_CORE false
+
+lando wp config set \
+    --path=/app/wordpress/ \
+    --type=constant \
+    --raw \
+    WP_DEBUG true
+
+lando wp db reset \
+    --path=/app/wordpress/ \
+    --yes
+
+wp_url="$(lando info | grep -A4 '"urls": \[$' | tail -n 1 | cut -d'"' -f2)"
+lando wp core install \
+    --path=/app/wordpress/ \
+    --url="$wp_url" \
+    '--title="My Test Site"' \
+    --admin_user="admin" \
+    --admin_password="admin" \
+    --admin_email="admin@example.com" \
+    --skip-email
+
+echo "Testing site URL: $wp_url"
+
+lando composer install
+
+mkdir -p ./wordpress/wp-content/plugins/bea-sanitize-filename/
+cp bea-sanitize-filename.php ./wordpress/wp-content/plugins/bea-sanitize-filename/bea-sanitize-filename.php
+
+echo
+echo "Test site is ACTIVE: $wp_url"
+echo "username: admin"
+echo "password: admin"
--- tests/acceptance/UploadCest.php
+++ tests/acceptance/UploadCest.php
@@ -6,50 +6,46 @@ class UploadCest {
 		$I->loginAsAdmin();
 	}
 
-	public function _after( AcceptanceTester $I ) {
-	}
-
-	// tests
 	public function TestABCD( AcceptanceTester $I ) {
-		$I->wantTo('Test the upload abcd file');
-		$I->amOnPage('wp-admin/media-new.php');
+		$I->wantTo( 'Test the upload abcd file' );
+		$I->amOnPage( 'wp-admin/media-new.php?browser-uploader' );
 
-		$I->attachFile('#async-upload', 'ABCDÇ.jpg');
-		$I->click('#file-form input[type="submit"]');
-		$I->amOnUrl('http://wp.localhost/wp-admin/upload.php?mode=list');
-		$I->see('abcdc.jpg');
+		$I->attachFile( '#async-upload', 'ABCDÇ.jpg' );
+		$I->click( '#file-form input[type="submit"]' );
+		$I->amOnPage( 'wp-admin/upload.php?mode=list' );
+		$I->see( 'abcdc.jpg' );
 	}
 
 	public function TestUnderscore( AcceptanceTester $I ) {
-		$I->wantTo('Test the upload underscore file');
-		$I->amOnPage('wp-admin/media-new.php');
+		$I->wantTo( 'Test the upload underscore file' );
+		$I->amOnPage( 'wp-admin/media-new.php?browser-uploader' );
 
-		$I->attachFile('#async-upload', 'filename_with_underscore.jpg');
-		$I->click('#file-form input[type="submit"]');
-		$I->amOnUrl('http://wp.localhost/wp-admin/upload.php?mode=list');
-		$I->see('filename-with-underscore.jpg');
+		$I->attachFile( '#async-upload', 'filename_with_underscore.jpg' );
+		$I->click( '#file-form input[type="submit"]' );
+		$I->amOnPage( 'wp-admin/upload.php?mode=list' );
+		$I->see( 'filename-with-underscore.jpg' );
 
 	}
 
 	public function TestSpaces( AcceptanceTester $I ) {
-		$I->wantTo('Test the upload spaced file');
-		$I->amOnPage('wp-admin/media-new.php');
+		$I->wantTo( 'Test the upload spaced file' );
+		$I->amOnPage( 'wp-admin/media-new.php?browser-uploader' );
 
-		$I->attachFile('#async-upload', 'testing  filename with    lots of  spaces    .jpg');
-		$I->click('#file-form input[type="submit"]');
-		$I->amOnUrl('http://wp.localhost/wp-admin/upload.php?mode=list');
-		$I->see('testing-filename-with-lots-of-spaces.jpg');
+		$I->attachFile( '#async-upload', 'testing  filename with    lots of  spaces    .jpg' );
+		$I->click( '#file-form input[type="submit"]' );
+		$I->amOnPage( 'wp-admin/upload.php?mode=list' );
+		$I->see( 'testing-filename-with-lots-of-spaces.jpg' );
 
 	}
 
 	public function TestSpecials( AcceptanceTester $I ) {
-		$I->wantTo('Test the upload special chars file');
-		$I->amOnPage('wp-admin/media-new.php');
+		$I->wantTo( 'Test the upload special chars file' );
+		$I->amOnPage( 'wp-admin/media-new.php?browser-uploader' );
 
-		$I->attachFile('#async-upload', '’‘“”«»‹›—€[]{}.jpg');
-		$I->click('#file-form input[type="submit"]');
-		$I->amOnUrl('http://wp.localhost/wp-admin/upload.php?mode=list');
-		$I->see('unnamed-file.jpg');
+		$I->attachFile( '#async-upload', '’‘“”«»‹›—€[]{}.jpg' );
+		$I->click( '#file-form input[type="submit"]' );
+		$I->amOnPage( 'wp-admin/upload.php?mode=list' );
+		$I->see( 'unnamed-file.jpg' );
 
 	}
 }
