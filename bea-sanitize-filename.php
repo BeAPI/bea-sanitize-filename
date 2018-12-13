0af525afa9b3ddc38817a1c61024bc66a48d31d5
Maxime Culea <culea.maxime@gmail.com>
2018-12-13 09:41:44 +0100

    Classify, refactor & add actions on handle media sideload media

--- bea-sanitize-filename.php
+++ bea-sanitize-filename.php
@@ -1,17 +1,15 @@
 <?php
 /*
  Plugin Name: BEA - Sanitize Filename
- Version: 2.0.6
- Plugin URI: https://github.com/BeAPI/bea-sanitize-filename
+ Version: 2.1.0
+ Plugin URI: https://fr.wordpress.org/plugins/bea-sanitize-filename
  Description: Remove all punctuation and accents from the filename of uploaded files.
  Author: Be API Technical team
  Author URI: https://beapi.fr
  Domain Path: languages
  Text Domain: bea-sanitize-filename
  Contributors: Amaury Balmer, Maxime Culea
-  
  --------
-  
  Copyright 2018 Be API Technical team (human@beapi.fr)
 
  This program is free software; you can redistribute it and/or modify
@@ -29,59 +27,100 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-function bea_sanitize_file_name_chars( $special_chars = array() ) {
-	// Special caracters
-	$special_chars = array_merge( array( '’', '‘', '“', '”', '«', '»', '‹', '›', '—', '€', '©' ), $special_chars );
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class BEA_Sanitize_Filename {
+	function __construct() {
+		add_filter( 'sanitize_file_name_chars', [ $this, 'add_file_name_chars' ] );
+
+		add_filter( 'sanitize_file_name', [ __CLASS__, 'sanitize_file_name' ] );
+		// Manage library upload
+		add_filter( 'wp_handle_upload_prefilter', [ $this, 'handle_upload_file' ] );
+		// Manage code library insertion
+		add_filter( 'wp_handle_sideload_prefilter', [ $this, 'handle_upload_file' ] );
+	}
+
 	/**
-	 * Accentued caracters
-	 * @see   https://github.com/BeAPI/bea-sanitize-filename/issues/8
-	 * @since 2.0.6
+	 * Handle special chars which are not in core
+	 * Accentuated characters are removed with remove_accents()
+	 *
+	 * @param array $special_chars
+	 *
+	 * @author Maxime CULEA
+	 *
+	 * @return array
 	 */
-	$special_chars = array_merge( array( 'À','Á','Â','Ã','Ä','Å','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ò','Ó','Ô','Õ','Ö','Ù','Ú','Û','Ü','Ý','à','á','â','ã','ä','å','ç','è','é','ê','ë','ì','í','î','ï','ð','ò','ó','ô','õ','ö','ù','ú','û','ü','ý','ÿ' ), $special_chars );
+	function add_file_name_chars( $special_chars = [] ) {
+		// Special caracters
+		$special_chars = array_merge( [ '’', '‘', '“', '”', '«', '»', '‹', '›', '—', '€', '©' ], $special_chars );
 
-	return $special_chars;
-}
-add_filter( 'sanitize_file_name_chars', 'bea_sanitize_file_name_chars', 10, 1 );
-
-/**
- * Filters the filename by adding more rules :
- * - only lowercase
- * - replace _ by -
- *
- * @since 1.0.1
- *
- * @param string $file_name
- *
- * @return string
- */
-function bea_sanitize_file_name( $file_name = '' ) {
-	// Empty filename
-	if ( empty($file_name) ) {
-		return $file_name;
+		return $special_chars;
 	}
 
-	// get extension
-	preg_match( '/\.[^\.]+$/i', $file_name, $ext );
+	/**
+	 * Filters the filename by adding more rules :
+	 * - only lowercase
+	 * - replace _ by -
+	 * - remove accents with sanitize filename
+	 *
+	 * @since 1.0.1
+	 *
+	 * @param string $file_name
+	 *
+	 * @return string
+	 */
+	static function sanitize_file_name( $file_name = '' ) {
+		// Empty filename
+		if ( empty( $file_name ) ) {
+			return $file_name;
+		}
 
-	// No extension, go out ?
-	if ( ! isset( $ext[0] ) ) {
-		return $file_name;
+		// get extension
+		preg_match( '/\.[^\.]+$/i', $file_name, $ext );
+
+		// No extension, go out ?
+		if ( ! isset( $ext[0] ) ) {
+			return $file_name;
+		}
+
+		// Get only first part
+		$ext = $ext[0];
+
+		// work only on the filename without extension
+		$file_name = str_replace( $ext, '', $file_name );
+
+		/**
+		 * 1 : _ with - and only lowercase
+		 * @see wp-includes/formatting.php\sanitize_title_with_dashes()
+		 *
+		 * 2 : remove accents
+		 * @see : wp-includes/formatting.php\remove_accents()
+		 */
+		$file_name = sanitize_title( $file_name );
+
+		// replace _ by -
+		$file_name = str_replace( '_', '-', $file_name );
+
+		// Return sanitized file name
+		return $file_name . $ext;
 	}
 
-	// Get only first part
-	$ext = $ext[0];
+	/**
+	 * On file upload, add the global sanitize file name process which also include ours
+	 *
+	 * @param $file
+	 *
+	 * @author Maxime CULEA
+	 *
+	 * @return mixed
+	 */
+	function handle_upload_file( $file ) {
+		$file['name'] = sanitize_file_name( $file['name'] );
 
-	// work only on the filename without extension
-	$file_name = str_replace( $ext, '', $file_name );
-
-	// only lowercase
-	// remove accents
-	$file_name = sanitize_title( $file_name );
-
-	// replace _ by -
-	$file_name = str_replace( '_', '-', $file_name );
-
-	// Return sanitized file name
-	return $file_name . $ext;
+		return $file;
+	}
 }
-add_filter( 'sanitize_file_name', 'bea_sanitize_file_name', 10, 1 );
+
+new BEA_Sanitize_Filename();
