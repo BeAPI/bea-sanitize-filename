dbac000eabf776c275c6ae18839a8d96afff7c8c
petitphp <c.boirie@gmail.com>
2019-07-02 23:20:19 +0200

    remove special chars already handle by remove_accents function

--- bea-sanitize-filename.php
+++ bea-sanitize-filename.php
@@ -29,15 +29,16 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+/**
+ * Add more special chars.
+ *
+ * @param array $special_chars
+ *
+ * @return array
+ */
 function bea_sanitize_file_name_chars( $special_chars = array() ) {
 	// Special caracters
 	$special_chars = array_merge( array( '’', '‘', '“', '”', '«', '»', '‹', '›', '—', '€', '©' ), $special_chars );
-	/**
-	 * Accentued caracters
-	 * @see   https://github.com/BeAPI/bea-sanitize-filename/issues/8
-	 * @since 2.0.6
-	 */
-	$special_chars = array_merge( array( 'À','Á','Â','Ã','Ä','Å','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ò','Ó','Ô','Õ','Ö','Ù','Ú','Û','Ü','Ý','à','á','â','ã','ä','å','ç','è','é','ê','ë','ì','í','î','ï','ð','ò','ó','ô','õ','ö','ù','ú','û','ü','ý','ÿ' ), $special_chars );
 
 	return $special_chars;
 }
